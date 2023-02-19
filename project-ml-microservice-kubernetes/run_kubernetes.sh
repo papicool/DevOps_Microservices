42a2a462106fb0dd1ed35a26c274d24908838571
EC2 Default User <ec2-user@ip-172-31-14-116.ec2.internal>
2023-02-19 00:55:54 +0000

    my last commit from my ec2

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="papamfall/sklear:latest"
+
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run sklearn --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward sklearn 8000:80
